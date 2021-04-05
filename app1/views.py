7b87b4eb03af1a8fc6486e257e3ed6b6e1f914b8
programmingtre <soundaryabeeranalli4@gmail.com>
2021-04-05 12:16:59 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>welcome to index of app1</h1>")
+
+def sample(request):
+    return render(request,"sample1.html")
+
+
+def sample_view(request):
+    return render(request,"sample1_app1.html")    
