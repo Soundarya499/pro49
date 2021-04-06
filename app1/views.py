29bf43bf6fcd9b1fb1abf46d534dd4b2c05e9a90
programmingtre <soundaryabeeranalli4@gmail.com>
2021-04-06 10:28:15 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -10,4 +10,26 @@ def sample(request):
 
 
 def sample_view(request):
-    return render(request,"sample1_app1.html")    
+    return render(request,"sample1_app1.html")  
+
+def carry_data(request,data):
+    return HttpResponse(f'<h1>the recieved data is {data}</h1>')
+
+def facto(request,num):
+    fact=1
+    for i in range(int(num),1,-1):
+        fact*=i
+    return HttpResponse(f'<h2>the factorial of {num} is {fact}</h2>')
+
+def add(request,num1,num2):
+    try:
+        num1=float(num1)
+    except:
+        num1=float(num1)
+    try:
+        num2=int(num2)
+    except:
+        num2=float(num2)
+    return HttpResponse(num1+num2)
+
+
